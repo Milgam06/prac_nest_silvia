ca2b4810f115a80eaea1362e38a0dc5d6058c98b
Milgam06 <milgamfruit@gmail.com>
2024-11-25 17:27:06 +0900

    chore: delete unusable files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+
+import { join } from "path";
+import { UserResolver } from "./user_gql/user_gql.resolver";
+import { UserGqlService } from "./user_gql/user_gql.service";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [TasksModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      csrfPrevention: false,
+    }),
+    PrismaModule,
+  ],
+  providers: [UserResolver, UserGqlService],
 })
 export class AppModule {}
